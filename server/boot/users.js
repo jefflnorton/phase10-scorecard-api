a5eaab66b1fac2ddffdc4a69c01dc2e29cd73c79
jefflnorton <jefflnorton@gmail.com>
2016-08-28 18:39:43 -0400

    Boot script for admin user

---
+++ server/boot/users.js
@@ -0,0 +1,32 @@
+module.exports = function(app) {
+  var User = app.models.user;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+
+  User.create({
+    username: 'admin',
+    email: 'jefflnorton@gmail.com',
+    password: 'password'
+  }, function(err, user) {
+    if (err) throw err;
+
+    console.log('Created admin user:', user);
+
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) throw err;
+
+      console.log('Created admin role:', role);
+
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: user.id
+      }, function(err, principal) {
+        if (err) throw err;
+
+        console.log('Created admin principal:', principal);
+      });
+    });
+  });
+};
